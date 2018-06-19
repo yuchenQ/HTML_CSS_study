623aeffc8270fe1e575e6bb639cba388f8a2ed3d
yuhcenQ <qiaoycmelbourne@gmail.com>
2018-06-19 17:28:21 +1000

    image

---
+++ image/script.js
@@ -0,0 +1,9 @@
+// when we click on the thumbnail we set the src attribute of the
+// big image to be the same as the src of the image we have clicked on
+// ("this"). This loads the same image file into the big image
+
+$(document).ready(() => {
+  $(".crop-img").click(function() {
+    $("#bigImage").attr('src', $(this).attr('src'));
+  });
+});
